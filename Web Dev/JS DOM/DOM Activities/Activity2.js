b7595187dad3e5a723bda5355760d4f722194d20
Siddhant Herale <heralesiddhant@gmail.com>
2025-09-19 09:57:24 +0530

    svg and DOM

--- Web Dev/JS DOM/DOM Activities/Activity2.js
+++ Web Dev/JS DOM/DOM Activities/Activity2.js
@@ -1,19 +1,3 @@
-// let charCount = document.getElementById('charCount');
-
-// let count = 0;
-// let char;
-
-
-// function char_count(count) {
-//     while(count <= 100) {
-//         if ((char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z')) {
-//             count++;
-//         }
-//         charCount.innerText = `${count}`;
-//     }
-// }
-// char_count();
-
 // Method 1:
 let message = document.getElementById('message');
 message.setAttribute('maxlength', '100');
