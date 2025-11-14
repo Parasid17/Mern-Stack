5d3d1c08c7f7ae2d4b9f59ffe51213e4ab3ce5f5
Siddhant Herale <heralesiddhant@gmail.com>
2025-11-14 10:05:11 +0530

    Fetch in JS

---
+++ Web Dev/Async JS/Fetch/fetch.js
@@ -0,0 +1,19 @@
+// fetch
+// to make http request
+
+
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+// .then((res) => {
+//     return res.json();
+// })
+// .then((data) => {
+//     console.log(data[0]);
+// })
+// .catch((err) => {
+//     console.log(err);
+// })
+
+
+// localhost:3000   -> fronend
+// example.com/server   -> backend
